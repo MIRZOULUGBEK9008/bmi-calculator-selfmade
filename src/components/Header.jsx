aa38b7e431a8ce56179363635bce8cb7ee0799c4
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-05-11 11:01:43 +0500

    Add site-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import SiteLogo from '../img/site-logo-min.svg'
 
 function Header() {
   return (
     <header className='site-header'>
-
+    <div className="site-header__container container">
+    <a className="site-header__logo logo" href="index.html">
+    <img className="logo__img" src={SiteLogo} alt="BMI logo" />
+    </a>
+    </div>
     </header>
     )
   }
